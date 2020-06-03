5f78b036194d277e9cd7064257cffdc00fe0bb90
tit-wiktorl <64896944+tit-wiktorl@users.noreply.github.com>
2020-06-04 01:04:06 +0200

    Add files via upload

---
+++ skrypcior.js
@@ -0,0 +1,72 @@
+function zmien1()
+{
+    document.getElementById('pies').src="npies.jpg";
+}
+function zmien2()
+{
+    document.getElementById('pies').src="psior.jpg";
+}
+function zmien3 ()
+{
+        document.getElementById('kot').src="nkot.jpg";
+}
+function zmien4 ()
+{
+        document.getElementById('kot').src="kot.jpg";
+}
+function zmien5 ()
+{
+    document.getElementById('shrek').src="nshrek.jpg";
+}
+function zmien6 ()
+{
+    document.getElementById('shrek').src="shrek.png";
+}n
+function zmien7 ()
+{
+    document.getElementById('mrowka').src="nmrowka.jpg";
+}
+function zmien8 ()
+{
+    document.getElementById('mrowka').src="mrowa.jpg";
+}
+function zmien9 ()
+{
+    document.getElementById('lew').src="nlew.jpg";
+}
+function zmien10 ()
+{
+    document.getElementById('lew').src="lew.jpg";
+}
+function zmien11 ()
+{
+    document.getElementById('bocian').src="nbocian.jpg";
+}
+function zmien12 ()
+{
+    document.getElementById('bocian').src="bocian.jpg";
+}
+function zmien13 ()
+{
+    document.getElementById('tygrys').src="ntygrys.jpg";
+}
+function zmien14 ()
+{
+    document.getElementById('tygrys').src="tygrys.jpg";
+}
+function zmien15 ()
+{
+    document.getElementById('golomp').src="ngolomp.jpg";
+}
+function zmien16 ()
+{
+    document.getElementById('golomp').src="golomp.jpg";
+}
+function zmien17 ()
+{
+    document.getElementById('slimak').src="nslimak.jpg";
+}
+function zmien18 ()
+{
+    document.getElementById('slimak').src="slimak.jpg";
+}
